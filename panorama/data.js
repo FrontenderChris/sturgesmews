d45e226e6a7dd2384a71dfadf8732bfcf41743d5
chriszhu <gansocc@gmail.com>
2020-02-28 12:50:04 +1300

    panorama update

--- panorama/data.js
+++ panorama/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-main-entrance",
-      "name": "Main Entrance",
+      "id": "0-entrance",
+      "name": "Entrance",
       "levels": [
         {
           "tileSize": 256,
@@ -26,182 +26,48 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.20794553691371753,
-          "pitch": 0.11808000157801679,
+          "yaw": -0.192955208584209,
+          "pitch": 0.10321541145978408,
           "rotation": 0,
-          "target": "2-courtyard-car-parks---01-"
-        },
-        {
-          "yaw": -1.4164957804603535,
-          "pitch": 0.06166595642836725,
-          "rotation": 0,
-          "target": "1-sturges-road"
+          "target": "1-courtyard-car-parks"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.7126517772654779,
-          "pitch": 0.04029267940676462,
-          "title": "Block E",
-          "text": "Lot 21-25: 2 bedrooms 70m2"
-        },
-        {
-          "yaw": -1.1267277927203239,
-          "pitch": -0.20243616346748183,
-          "title": "Block E",
-          "text": "Lot26-27: 3bedrooms 110m2"
-        },
-        {
-          "yaw": 0.7538796911235579,
-          "pitch": 0.10978810082642099,
+          "yaw": 0.7685195416929265,
+          "pitch": 0.0033960984878120826,
           "title": "Block D",
-          "text": "Lot 16-20: 2bedrooms 73m2"
-        }
-      ]
-    },
-    {
-      "id": "1-sturges-road",
-      "name": "Sturges Road",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "text": "LOT 16-20<div>2 Bedrooms 73m2</div>"
         },
         {
-          "tileSize": 512,
-          "size": 512
-        }
-      ],
-      "faceSize": 512,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.095395215817824,
-          "pitch": 0.10298539638098347,
-          "rotation": 0,
-          "target": "0-main-entrance"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 0.6559516374545868,
-          "pitch": -0.06399770619381506,
+          "yaw": -0.8550870526910082,
+          "pitch": -0.006320224264655394,
           "title": "Block E",
-          "text": "Lot 21-25: 2bedrooms 70ms"
+          "text": "LOT 21-25<div>2 Bedrooms 70m2</div>"
         },
         {
-          "yaw": 1.1239113657461104,
-          "pitch": 0.3871298672498007,
-          "title": "Sturges Road",
-          "text": "A short drive to access the North Western Motorway<br>"
-        },
-        {
-          "yaw": 0.06575446561248377,
-          "pitch": 0.1296081300088474,
+          "yaw": -1.1307657134164693,
+          "pitch": -0.20160026233209294,
           "title": "Block E",
-          "text": "Lot 26-27: 3Bedrooms 110m2"
+          "text": "LOT 26-27<div>3 Bedrooms 110m2</div>"
         },
         {
-          "yaw": -0.7107442248745741,
-          "pitch": -0.036273948300610925,
-          "title": "Block F",
-          "text": "Lot28-33: 3 bedrooms 117.9m2"
-        },
-        {
-          "yaw": -1.4510752645312106,
-          "pitch": -0.0019781917988304087,
-          "title": "Railway",
-          "text": "<span style=\"background-color: rgba(58, 68, 84, 0.8);\">Three-minutes' distance to railway station by walking</span>"
-        }
-      ]
-    },
-    {
-      "id": "2-courtyard-car-parks---01-",
-      "name": "Courtyard Car Parks - 01 ",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
-      ],
-      "faceSize": 512,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.5052536448437461,
-          "pitch": 0.09332487895700048,
-          "rotation": 0,
-          "target": "3-courtyard-car-parks---02"
-        },
-        {
-          "yaw": 3.101573873676017,
-          "pitch": 0.11522239593780448,
-          "rotation": 0,
-          "target": "0-main-entrance"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -2.9975471681507795,
-          "pitch": 0.11700092945055829,
+          "yaw": 1.7064990751894253,
+          "pitch": 0.05855406510541705,
           "title": "Sturges Road",
           "text": "A short drive to access the North Western Motorway"
         },
         {
-          "yaw": 1.768353474905414,
-          "pitch": 0.09509985978139746,
-          "title": "Block D",
-          "text": "Lot 16-20: 2bedrooms 73m2"
-        },
-        {
-          "yaw": 0.4468558463154686,
-          "pitch": -0.004037521624123741,
-          "title": "Block C",
-          "text": "Lot 11-15: 2bedrooms 70.6m2"
-        },
-        {
-          "yaw": -0.33211232463166596,
-          "pitch": 0.005381363717649634,
-          "title": "Block B",
-          "text": "Lot 6-10: 2bedrooms 70.6m2"
-        },
-        {
-          "yaw": -1.5221123361997293,
-          "pitch": -0.09041596819636055,
-          "title": "Block E",
-          "text": "Lot 21-25: 2bedrooms 70m2"
-        },
-        {
-          "yaw": -0.6814633675011841,
-          "pitch": -0.1623226810968248,
-          "title": "Block F",
-          "text": "Lot 28-33: 3bedrooms 117.9m2"
-        },
-        {
-          "yaw": 0.14661949607458524,
-          "pitch": -0.1493057021970685,
-          "title": "Block A",
-          "text": "Lot 1-5: 2bedrooms 73m2"
+          "yaw": -1.354342758475175,
+          "pitch": 0.03183912326941751,
+          "title": "Sturges Road",
+          "text": "Three minutes' distance to railway station by walking"
         }
       ]
     },
     {
-      "id": "3-courtyard-car-parks---02",
-      "name": "Courtyard Car Parks - 02",
+      "id": "1-courtyard-car-parks",
+      "name": "Courtyard Car Parks",
       "levels": [
         {
           "tileSize": 256,
@@ -211,9 +77,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
         }
       ],
-      "faceSize": 512,
+      "faceSize": 1024,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -221,54 +91,60 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.7653188998752896,
-          "pitch": 0.10771108142808394,
+          "yaw": 2.9155917420510056,
+          "pitch": 0.06711962147532624,
           "rotation": 0,
-          "target": "2-courtyard-car-parks---01-"
+          "target": "0-entrance"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -1.9510207847587822,
-          "pitch": 0.0017767637742220899,
-          "title": "Block E",
-          "text": "Lot 21-25: 3bedrooms 70m2"
-        },
-        {
-          "yaw": -0.8177429971285708,
-          "pitch": -0.018835543466860116,
-          "title": "Block E",
-          "text": "Lot 26-27: 3bedrooms 110m2"
-        },
-        {
-          "yaw": 0.3780666210857806,
-          "pitch": 0.020568339274737113,
-          "title": "Block F",
-          "text": "Lot 28-33: 3bedrooms 117.9m2"
-        },
-        {
-          "yaw": 1.463273943492302,
-          "pitch": 0.009184840831068186,
-          "title": "Block B",
-          "text": "Lot 6-10: 2bedrooms 70.6m2"
-        },
-        {
-          "yaw": 2.15798071140929,
-          "pitch": -0.024464262546501914,
-          "title": "Block A",
-          "text": "Lot 1-5: 2bedrooms 73m2"
-        },
-        {
-          "yaw": 2.801359827541255,
-          "pitch": -0.004357108489433159,
+          "yaw": 1.5446104687187745,
+          "pitch": 0.13202267489862685,
           "title": "Block C",
-          "text": "Lot 11-15: 2bedrooms 70.6m2"
+          "text": "LOT 11-15<div>2 Bedrooms 73m2</div>"
         },
         {
-          "yaw": -2.9871260137068205,
-          "pitch": -0.08979078921928618,
+          "yaw": 2.447863926275251,
+          "pitch": 0.07988588486893278,
           "title": "Block D",
-          "text": "Lot 16-20: 2bedrooms 73m2"
+          "text": "LOT 16-20<div>2 Bedrooms 73 m2</div>"
+        },
+        {
+          "yaw": -2.519117524393069,
+          "pitch": 0.18671524298641629,
+          "title": "Block E",
+          "text": "LOT 21-25<div>2 Bedrooms 70m2</div>"
+        },
+        {
+          "yaw": -1.81834308011873,
+          "pitch": -0.023690911028513284,
+          "title": "Block E",
+          "text": "LOT 26-27<div>3 Bedrooms 110m2</div>"
+        },
+        {
+          "yaw": -0.8670157601497266,
+          "pitch": 0.005333284643901592,
+          "title": "Block F",
+          "text": "LOT 28-33<div>3 Bedrooms 117m2</div>"
+        },
+        {
+          "yaw": -0.19761511097126494,
+          "pitch": 0.17520975707621034,
+          "title": "Block B",
+          "text": "LOT 6-10<div>2 Bedrooms 70.6m2</div>"
+        },
+        {
+          "yaw": 0.6173295817814299,
+          "pitch": 0.14154715434587217,
+          "title": "Block A",
+          "text": "LOT 1-5<div>2 Bedrooms 73m2</div>"
+        },
+        {
+          "yaw": 3.0096456950360277,
+          "pitch": 0.06788274714216946,
+          "title": "Sturges Road",
+          "text": "Main Entrance"
         }
       ]
     }
